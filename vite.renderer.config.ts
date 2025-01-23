8b30544325d6b49a0207ef17b9118200f6df30ec
Maiko Tan <maiko.tan.coding@gmail.com>
2025-01-23 13:45:40 +0800

    feat: update main and preload entry points, add main process logic

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig } from 'vite';
+import UnoCSS from 'unocss/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [UnoCSS()],
+})
